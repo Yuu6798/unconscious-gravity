9986fe8f4d4384f976b335d04d3e5af66736d709
Yuu6798 <kkoo6798@gmail.com>
2025-04-20 10:53:14 +0900

    test_por_model.py の更新

--- tests/test_por_model.py
+++ tests/test_por_model.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import unittest
 from models.por_model import PoRModel
