6303f81296c50cfb88abc3f2795bdbd08f191dba
Yuu6798 <kkoo6798@gmail.com>
2025-04-20 10:52:51 +0900

    test_por_model_calculations.py の更新

--- tests/test_por_model_calculations.py
+++ tests/test_por_model_calculations.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest import numpy as np import math
 
