1ffa7af7f311c608845038aebdc1ab37e5861c1c
Yuu6798 <kkoo6798@gmail.com>
2025-04-29 00:23:49 +0900

    setup.py の更新

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools 
-import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name="unconscious_gravity_exp",
     version="0.0.9",
     description="PoR log diagnostics CLI",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(where="src"),      # ★ src/以下をパッケージとして拾う
+    package_dir={"": "src"},                   # ★ ルートからsrc/にマッピングする
     install_requires=[
         "numpy",
         "pandas",
